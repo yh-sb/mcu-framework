0a682f877c383d07e8cc2af8c3a85091f2fe433c
r44083 <r44083.r@gmail.com>
2018-06-18 22:02:55 +0300

    Fit code in 80 character delimiter

--- src/drv/sd/sd_spi.hpp
+++ src/drv/sd/sd_spi.hpp
@@ -19,7 +19,8 @@ class sd_spi : public sd
 		void select(bool is_selected) override;
 		int8_t init_sd() override;
 		void set_speed(uint32_t speed) override;
-		int8_t send_cmd(cmd_t cmd, uint32_t arg, resp_t resp_type, uint8_t *resp) override;
+		int8_t send_cmd(cmd_t cmd, uint32_t arg, resp_t resp_type,
+			uint8_t *resp) override;
 		int8_t read_data(void *data, uint16_t size) override;
 		int8_t write_data(void *data, uint16_t size) override;
 		
