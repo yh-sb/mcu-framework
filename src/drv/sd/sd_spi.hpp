0e87339c49e68c31ad31f4a6826d866da899d6f2
r44083 <r44083.r@gmail.com>
2018-06-18 22:00:18 +0300

    Added 'override' checking mechanism for sd spi class

--- src/drv/sd/sd_spi.hpp
+++ src/drv/sd/sd_spi.hpp
@@ -16,12 +16,12 @@ class sd_spi : public sd
 		~sd_spi();
 	
 	private:
-		virtual void select(bool is_selected);
-		virtual int8_t init_sd();
-		virtual void set_speed(uint32_t speed);
-		virtual int8_t send_cmd(cmd_t cmd, uint32_t arg, resp_t resp_type, uint8_t *resp);
-		virtual int8_t read_data(void *data, uint16_t size);
-		virtual int8_t write_data(void *data, uint16_t size);
+		void select(bool is_selected) override;
+		int8_t init_sd() override;
+		void set_speed(uint32_t speed) override;
+		int8_t send_cmd(cmd_t cmd, uint32_t arg, resp_t resp_type, uint8_t *resp) override;
+		int8_t read_data(void *data, uint16_t size) override;
+		int8_t write_data(void *data, uint16_t size) override;
 		
 		int8_t wait_ready();
 		
