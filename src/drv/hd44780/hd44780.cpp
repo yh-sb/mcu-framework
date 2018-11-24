a8f256f24331e15584dd6028774e18ccf372d3e9
r44083 <r44083.r@gmail.com>
2018-11-24 11:12:48 +0200

    hd44780 driver was refactored. Added "clear display" api

--- src/drv/hd44780/hd44780.cpp
+++ src/drv/hd44780/hd44780.cpp
@@ -9,18 +9,73 @@
 using namespace drv;
 using namespace hal;
 
-#define X_MAX 19
-#define Y_MAX 3
+#define DDRAM1_MIN_ADDR 0
+#define DDRAM1_MAX_ADDR 39
+#define DDRAM2_MIN_ADDR 64
+#define DDRAM2_MAX_ADDR 103
 
-hd44780::hd44780(gpio &e, gpio &rw, gpio &rs, gpio &db4, gpio &db5, gpio &db6,
+enum cmd_t
+{
+	CLEAR_DISPLAY                  = 1 << 0,
+	RETURN_HOME                    = 1 << 1,
+	ENTRY_MODE_SET                 = 1 << 2,
+	DISPLAY_ON_OFF_CONTROL         = 1 << 3,
+	CURSOR_OR_DISPLAY_SHIFT        = 1 << 4,
+	FUNCTION_SET                   = 1 << 5,
+	SET_CGRAM_ADDRESS              = 1 << 6,
+	SET_DDRAM_ADDRESS              = 1 << 7
+	
+	/* Read only command or command which doesn't require data lines
+	READ_BUSY_FLAG_AND_ADDRESS (RW = 1)
+	WRITE_DATA_TO_ADDRESS      (RW = 0)
+	READ_DATA_FROM_RAM         (RW = 1, RS = 1)
+	*/
+};
+
+// Bits for ENTRY_MODE_SET command
+enum entry_mode_set_bits_t
+{
+	I_D_BIT = 1 << 1, // Increment/Decrement DDRAM address (cursor position):
+	                  // 0 - decrement, 1 - increment
+	S_BIT   = 1 << 0  // Shift the dispaly with each new character
+};
+
+// Bits for DISPLAY_ON_OFF_CONTROL command
+enum display_on_off_control_bits_t
+{
+	D_BIT = 1 << 2, // On/off entire display
+	C_BIT = 1 << 1, // On/off cursor
+	B_BIT = 1 << 0  // On/off blinking cursor position
+};
+
+// Bits for CURSOR_OR_DISPLAY_SHIFT command
+enum cursor_or_display_shift_bits_t
+{
+	S_C_BIT = 1 << 3, // Shift display or cursor: 0 - cursor, 1 - display
+	R_L_BIT = 1 << 2  // Direction of shift: 0 - to the left, 1 - to the right
+};
+
+// Bits for FUNCTION_SET command
+enum function_set_bits_t
+{
+	DL_BIT  = 1 << 4, // Interface data length: 0 - 4 bit, 1 - 8 bit
+	N_BIT   = 1 << 3, // Number of display lines: 0 - one line, 1 - two line
+	F_BIT   = 1 << 2, // Character font: 0 - 5x8, 1 - 5x10
+	FT1_BIT = 1 << 1, // Font table: (FT1:FT0)
+	FT0_BIT = 1 << 0, //            00 - ENGLISH_JAPANESE
+	                  //            01 - WESTERN EUROPEAN
+	                  //            10 - ENGLISH_RUSSIAN
+	                  //            11 - N/A
+};
+
+hd44780::hd44780(gpio &rs, gpio &rw, gpio &e, gpio &db4, gpio &db5, gpio &db6,
 	gpio &db7, tim &tim):
-	_e(e),
-	_rw(rw),
 	_rs(rs),
+	_rw(rw),
+	_e(e),
 	_db{&db4, &db5, &db6, &db7},
 	_tim(tim),
-	_x(0),
-	_y(0)
+	_ddram_addr(0)
 {
 	ASSERT(_e.mode() == gpio::mode::DO);
 	ASSERT(_rw.mode() == gpio::mode::DO);
@@ -28,11 +83,14 @@ hd44780::hd44780(gpio &e, gpio &rw, gpio &rs, gpio &db4, gpio &db5, gpio &db6,
 	
 	for(uint8_t i = 4; (i < sizeof(_db)/sizeof(_db[0])) && _db[i]; i++)
 		ASSERT(_db[i]->mode() == gpio::mode::DO);
+	
+	_lock = xSemaphoreCreateBinary();
+	ASSERT(_lock);
 }
 
 hd44780::~hd44780()
 {
-	
+	vSemaphoreDelete(_lock);
 }
 
 void hd44780::init()
@@ -40,20 +98,21 @@ void hd44780::init()
 	_rw.set(0);
 	_rs.set(0);
 	
-	write_4bit(0b00000011);
+	write_4bit(FUNCTION_SET >> 4);
 	delay(4100);
-	write_4bit(0b00000011);
+	write_4bit(FUNCTION_SET >> 4);
 	delay(100);
-	write_4bit(0b00000011);
+	write_4bit(FUNCTION_SET >> 4);
 	
-	write_4bit(0x28 >> 4);
-	write(CMD, 0x28); // 2 lines, 5x8 character matrix
-	write(CMD, 0x0C); // Display ctrl: Disp=ON, Curs/Blnk=OFF
-	write(CMD, 0x06); // Entry mode: Move right, no shift
+	write_4bit((FUNCTION_SET | N_BIT) >> 4);
+	write(CMD, FUNCTION_SET | N_BIT);
 	
-	//write(CMD, 0x01); // Clear
-	x(0);
-	delay(1530);
+	write(CMD, DISPLAY_ON_OFF_CONTROL | D_BIT);
+	
+	write(CMD, CLEAR_DISPLAY);
+	delay(6200); // OLED display requires 6,2 ms rather than 1,53 ms
+	
+	write(CMD, ENTRY_MODE_SET | I_D_BIT);
 }
 
 void hd44780::print(const char *str)
@@ -70,22 +129,20 @@ void hd44780::print(char byte)
 	write(DATA, byte);
 }
 
-void hd44780::x(uint8_t x)
+void hd44780::ddram_addr(uint8_t addr)
 {
-	ASSERT(x <= X_MAX);
+	ASSERT((addr >= DDRAM1_MIN_ADDR && addr <= DDRAM1_MAX_ADDR) ||
+		(addr >= DDRAM2_MIN_ADDR && addr <= DDRAM2_MAX_ADDR));
 	
-	uint8_t y_shift[] = {0x00, 0x40, 0x14, 0x54};
+	_ddram_addr = addr;
 	
-	write(CMD, (y_shift[_y] + x) | 0b10000000);
+	write(CMD, SET_DDRAM_ADDRESS | _ddram_addr);
 }
 
-void hd44780::y(uint8_t y)
+void hd44780::clear()
 {
-	ASSERT(y <= Y_MAX);
-	
-	uint8_t y_shift[] = {0x00, 0x40, 0x14, 0x54};
-	
-	write(CMD, (y_shift[y] + _x) | 0b10000000);
+	write(CMD, CLEAR_DISPLAY);
+	delay(6200); // OLED display requires 6,2 ms rather than 1,53 ms
 }
 
 void hd44780::write_4bit(uint8_t half_byte)
@@ -108,9 +165,19 @@ void hd44780::write(write_t type, uint8_t byte)
 	write_4bit(byte);
 }
 
+static void tim_cb(tim *tim, void *ctx)
+{
+	SemaphoreHandle_t _lock = (SemaphoreHandle_t)ctx;
+	
+	BaseType_t hi_task_woken = 0;
+	xSemaphoreGiveFromISR(_lock, &hi_task_woken);
+	portYIELD_FROM_ISR(hi_task_woken);
+}
+
 void hd44780::delay(uint32_t us)
 {
 	_tim.us(us);
-	_tim.start_once(NULL, NULL);
-	while(_tim.is_running());
+	_tim.start_once(tim_cb, _lock);
+	
+	xSemaphoreTake(_lock, portMAX_DELAY);
 }
--- src/drv/hd44780/hd44780.hpp
+++ src/drv/hd44780/hd44780.hpp
@@ -6,12 +6,15 @@
 #include "gpio.hpp"
 #include "tim.hpp"
 
+#include "third_party/FreeRTOS/include/FreeRTOS.h"
+#include "third_party/FreeRTOS/include/semphr.h"
+
 namespace drv
 {
 class hd44780
 {
 	public:
-		hd44780(hal::gpio &e, hal::gpio &rw, hal::gpio &rs, hal::gpio &db4,
+		hd44780(hal::gpio &rs, hal::gpio &rw, hal::gpio &e, hal::gpio &db4,
 			hal::gpio &db5, hal::gpio &db6, hal::gpio &db7, hal::tim &tim);
 		
 		~hd44780();
@@ -21,36 +24,20 @@ class hd44780
 		void print(const char *str);
 		void print(char byte);
 		
-		void x(uint8_t x);
-		void y(uint8_t y);
+		void ddram_addr(uint8_t addr);
+		uint8_t ddram_addr() const { return _ddram_addr; }
 		
-		uint8_t x() const { return _x; }
-		uint8_t y() const { return _y; }
+		void clear();
 		
 	private:
-		hal::gpio &_e;
-		hal::gpio &_rw;
 		hal::gpio &_rs;
+		hal::gpio &_rw;
+		hal::gpio &_e;
 		hal::gpio *_db[4];
 		hal::tim &_tim;
 		
-		uint8_t _x;
-		uint8_t _y;
-		
-		/*
-		typedef enum
-		{
-			CMD_CLEAR_DISPLAY                  = 1 << 0,
-			CMD_RETRNU_HOME                    = 1 << 1,
-			CMD_SET_CURSOR_SHIFT               = (1 << 2) | (1 << 1),
-			CMD_ON_OFF_DISPLAY_SHIFT           = (1 << 2) | (1 << 0),
-			CMD_ON_OFF_DISPLAY                 = (1 << 3) | (1 << 2),
-			CMD_ON_OFF_CURSOR                  = (1 << 3) | (1 << 1),
-			CMD_ON_OFF_CURSOR_BLINK            = (1 << 3) | (1 << 0),
-			CMD_ON_OFF_CURSOR_OR_DISPLAY_SHIFT = 
-			CMD_ON_OFF_CURSOR_OR_DISPLAY_SHIFT = 
-		} cmd_t;
-		*/
+		uint8_t _ddram_addr;
+		SemaphoreHandle_t _lock;
 		
 		enum write_t
 		{
