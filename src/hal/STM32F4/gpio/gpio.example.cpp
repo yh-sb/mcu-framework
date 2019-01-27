1d37075268d1546e508e555e5d340d760307c520
r44083 <r44083.r@gmail.com>
2019-01-28 00:01:42 +0200

    Added hal gpio examples

---
+++ src/hal/STM32F4/gpio/gpio.example.cpp
@@ -0,0 +1,26 @@
+#include "common/assert.h"
+#include "gpio/gpio.hpp"
+#include "FreeRTOS.h"
+#include "task.h"
+
+using namespace hal;
+
+static void main_task(void *pvParameters)
+{
+	gpio *green_led = (gpio *)pvParameters;
+	while(1)
+	{
+		green_led->toggle();
+		vTaskDelay(500);
+	}
+}
+
+int main(void)
+{
+	static gpio green_led(3, 12, gpio::MODE_DO, 0);
+	
+	ASSERT(xTaskCreate(main_task, "main", configMINIMAL_STACK_SIZE * 1,
+		&green_led, tskIDLE_PRIORITY + 1, NULL) == pdPASS);
+	
+	vTaskStartScheduler();
+}
