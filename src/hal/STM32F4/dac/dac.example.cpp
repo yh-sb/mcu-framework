7d32539cb8982119781d51120d642d6a9bc53293
r44083 <r44083.r@gmail.com>
2019-01-27 22:41:52 +0200

    Added examples for hal dac

---
+++ src/hal/STM32F4/dac/dac.example.cpp
@@ -0,0 +1,54 @@
+#include "common/assert.h"
+#include "dac/dac.hpp"
+#include "gpio/gpio.hpp"
+#include "tim/tim.hpp"
+#include "FreeRTOS.h"
+#include "task.h"
+
+using namespace hal;
+
+static void main_task(void *pvParameters)
+{
+	gpio *green_led = (gpio *)pvParameters;
+	while(1)
+	{
+		green_led->toggle();
+		vTaskDelay(500);
+	}
+}
+
+static void tim_cb(tim *tim, void *ctx)
+{
+	dac *dac1 = (dac *)ctx;
+	
+	static const uint16_t sin[] =
+	{
+		2047, 2447, 2831, 3185, 3498, 3750, 3939, 4056,
+		4095, 4056, 3939, 3750, 3495, 3185, 2831, 2447,
+		2047, 1647, 1263,  909,  599,  344,  155,   38,
+		   0,   38,  155,  344,  599,  909, 1263, 1647
+	};
+	static uint8_t cnt = 0;
+	
+	dac1->set(sin[cnt++]);
+	if(cnt == (sizeof(sin) / sizeof(sin[0])))
+		cnt = 0;
+}
+
+int main(void)
+{
+	static gpio dac1_gpio(0, 4, gpio::MODE_AN, 0);
+	static gpio green_led(3, 12, gpio::MODE_DO, 0);
+	
+	static dac dac1(dac::DAC_1, dac::ALIGN_R_12, dac1_gpio);
+	
+	static tim tim6(tim::TIM_6);
+	tim6.cb(tim_cb, &dac1);
+	tim6.us(32); // Sine wave frequency is 1 kHz (sin[32])
+	tim6.start(true);
+	
+	ASSERT(xTaskCreate(main_task, "main", configMINIMAL_STACK_SIZE * 1,
+		&green_led, tskIDLE_PRIORITY + 1, NULL) == pdPASS);
+	
+	vTaskStartScheduler();
+}
