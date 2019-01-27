071ae0388b2a766ba7bac0eab9e4a75d540ce15e
r44083 <r44083.r@gmail.com>
2019-01-28 00:59:41 +0200

    Added hal pwm example

---
+++ src/hal/STM32F4/pwm/pwm.example.cpp
@@ -0,0 +1,35 @@
+#include "common/assert.h"
+#include "gpio/gpio.hpp"
+#include "pwm/pwm.hpp"
+#include "tim/tim.hpp"
+#include "FreeRTOS.h"
+#include "task.h"
+
+using namespace hal;
+
+static void main_task(void *pvParameters)
+{
+	gpio *green_led = (gpio *)pvParameters;
+	while(1)
+	{
+		green_led->toggle();
+		vTaskDelay(500);
+	}
+}
+
+int main(void)
+{
+	static gpio green_led(3, 12, gpio::MODE_DO, 0);
+	static gpio pwm2_ch2_gpio(0, 1, gpio::MODE_AF, 0);
+	
+	static pwm pwm2_ch2(tim::TIM_2, pwm::CH_2, pwm::MODE_NONINVERTED,
+		pwm2_ch2_gpio);
+	pwm2_ch2.freq(100000);
+	pwm2_ch2.duty(20);
+	pwm2_ch2.start();
+	
+	ASSERT(xTaskCreate(main_task, "main", configMINIMAL_STACK_SIZE * 1, &green_led,
+		tskIDLE_PRIORITY + 1, NULL) == pdPASS);
+	
+	vTaskStartScheduler();
+}
