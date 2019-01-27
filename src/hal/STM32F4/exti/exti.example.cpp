a5ca30f0908209717f70b4a531578e04e93c4c21
r44083 <r44083.r@gmail.com>
2019-01-27 23:41:13 +0200

    Added hal exti example

---
+++ src/hal/STM32F4/exti/exti.example.cpp
@@ -0,0 +1,40 @@
+#include "common/assert.h"
+#include "gpio/gpio.hpp"
+#include "exti/exti.hpp"
+#include "FreeRTOS.h"
+#include "task.h"
+
+using namespace hal;
+
+static void main_task(void *pvParameters)
+{
+	gpio *green_led = (gpio *)pvParameters;
+	while(1)
+	{
+		green_led->toggle();
+		vTaskDelay(500);
+	}
+}
+
+static void exti_cb(exti *exti, void *ctx)
+{
+	gpio *blue_led = (gpio *)ctx;
+	
+	blue_led->toggle();
+}
+
+int main(void)
+{
+	static gpio green_led(3, 12, gpio::MODE_DO, 0);
+	static gpio blue_led(3, 15, gpio::MODE_DO, 0);
+	static gpio exti1_gpio(0, 0, gpio::MODE_DI, 0);
+	
+	static exti exti1(exti1_gpio, exti::TRIGGER_FALLING);
+	exti1.cb(exti_cb, &blue_led);
+	exti1.on();
+	
+	ASSERT(xTaskCreate(main_task, "main", configMINIMAL_STACK_SIZE * 1,
+		&green_led, tskIDLE_PRIORITY + 1, NULL) == pdPASS);
+	
+	vTaskStartScheduler();
+}
