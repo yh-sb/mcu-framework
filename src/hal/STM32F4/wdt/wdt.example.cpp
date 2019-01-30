c75df50a3ae31ef7757173393c5bd6d307e70dd5
r44083 <r44083.r@gmail.com>
2019-01-30 02:06:31 +0200

    Added usage example for hal wdt

---
+++ src/hal/STM32F4/wdt/wdt.example.cpp
@@ -0,0 +1,32 @@
+#include "common/assert.h"
+#include "gpio/gpio.hpp"
+#include "wdt/wdt.hpp"
+#include "FreeRTOS.h"
+#include "task.h"
+
+using namespace hal;
+
+static void main_task(void *pvParameters)
+{
+	gpio *green_led = (gpio *)pvParameters;
+	while(1)
+	{
+		wdt::reload();
+		
+		green_led->toggle();
+		vTaskDelay(500);
+	}
+}
+
+int main(void)
+{
+	static gpio green_led(3, 12, gpio::MODE_DO, 0);
+	
+	wdt::init(1000);
+	wdt::on();
+	
+	ASSERT(xTaskCreate(main_task, "main", configMINIMAL_STACK_SIZE * 1,
+		&green_led, tskIDLE_PRIORITY + 1, NULL) == pdPASS);
+	
+	vTaskStartScheduler();
+}
