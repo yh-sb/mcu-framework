7cd9ac9c81ca38af0d23d0f306c60de04265c75e
r44083 <r44083.r@gmail.com>
2019-02-02 14:44:34 +0200

    Updated syslog utility example

--- src/ul/syslog/syslog.example.cpp
+++ src/ul/syslog/syslog.example.cpp
@@ -1,8 +1,8 @@
-#include "gpio/gpio.hpp"
-#include "uart/uart.hpp"
 #include "common/assert.h"
+#include "gpio/gpio.hpp"
+#include "dma/dma.hpp"
+#include "uart/uart.hpp"
 #include "ul/syslog/syslog.hpp"
-#include "ul/list/list.hpp"
 #include "FreeRTOS.h"
 #include "task.h"
 
@@ -29,16 +29,16 @@ static void uart_write(uint8_t *buff, size_t size, void *ctx)
 int main(void)
 {
 	// Example for STM32F4DISCOVERY development board
-	static gpio green_led(3, 12, gpio::mode::DO, 0);
-	static gpio uart3_tx_gpio(3, 8, gpio::mode::AF, 0);
-	static gpio uart3_rx_gpio(1, 11, gpio::mode::AF, 0);
+	static gpio green_led(3, 12, gpio::MODE_DO, 0);
+	static gpio uart3_tx_gpio(3, 8, gpio::MODE_AF, 0);
+	static gpio uart3_rx_gpio(1, 11, gpio::MODE_AF, 0);
 	
 	static dma uart3_tx_dma(dma::dma_t::DMA_1, dma::stream_t::STREAM_3,
 		dma::ch_t::CH_4, dma::dir_t::DIR_MEM_TO_PERIPH, dma::inc_size_t::INC_SIZE_8);
 	static dma uart3_rx_dma(dma::dma_t::DMA_1, dma::stream_t::STREAM_1,
 		dma::ch_t::CH_4, dma::dir_t::DIR_PERIPH_TO_MEM, dma::inc_size_t::INC_SIZE_8);
 	
-	static uart uart3(UART_3, 115200, UART_STOPBIT_1, UART_PARITY_NONE,
+	static uart uart3(uart::UART_3, 115200, uart::STOPBIT_1, uart::PARITY_NONE,
 		uart3_tx_dma, uart3_rx_dma, uart3_tx_gpio, uart3_rx_gpio);
 	
 	log().add_output(uart_write, &uart3);
