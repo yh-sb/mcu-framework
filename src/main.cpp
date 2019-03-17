11c463d0606ed6b738678328d52597d6467064b0
r44083 <r44083.r@gmail.com>
2019-03-17 14:16:03 +0200

    main.cpp update according to newer version of omef

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
-#include "gpio.hpp"
-
-#include "third_party/FreeRTOS/include/FreeRTOS.h"
-#include "third_party/FreeRTOS/include/task.h"
+#include "gpio/gpio.hpp"
+#include "FreeRTOS.h"
+#include "task.h"
 
 using namespace hal;
 
@@ -18,7 +17,7 @@ static void main_task(void *pvParameters)
 int main(void)
 {
 	// Example for STM32F4DISCOVERY development board
-	static gpio green_led(3, 12, gpio::mode::DO, 0);
+	static gpio green_led(3, 12, gpio::MODE_DO, 0);
 	
 	xTaskCreate(main_task, "main", configMINIMAL_STACK_SIZE * 1, &green_led,
 		tskIDLE_PRIORITY + 1, NULL);
