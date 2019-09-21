5d26fd59561cfaeab39f0952839aad05d65b37f8
r44083 <r44083.r@gmail.com>
2019-09-21 21:40:41 +0300

    Rework hal gpio for ESP8266

--- src/hal/ESP8266/gpio/gpio.cpp
+++ src/hal/ESP8266/gpio/gpio.cpp
@@ -4,11 +4,15 @@
 #include "gpio.hpp"
 #include "ESP8266_RTOS_SDK/components/esp8266/include/esp8266/pin_mux_register.h"
 #include "ESP8266_RTOS_SDK/components/esp8266/include/esp8266/gpio_register.h"
-#include "ESP8266_RTOS_SDK/components/esp8266/include/esp8266/eagle_soc.h"
+#include "ESP8266_RTOS_SDK/components/esp8266/include/esp8266/gpio_struct.h"
 
 using namespace hal;
 
-static const uint32_t mux_list[PIN_QTY] =
+constexpr uint8_t ports = 1;
+constexpr uint8_t pins = 17;
+constexpr uint8_t rtc_pin = 16;
+
+static const uint32_t mux_list[pins] =
 {
 	PERIPHS_IO_MUX_GPIO0_U,
 	PERIPHS_IO_MUX_U0TXD_U,
@@ -26,37 +30,36 @@ static const uint32_t mux_list[PIN_QTY] =
 	PERIPHS_IO_MUX_MTCK_U,
 	PERIPHS_IO_MUX_MTMS_U,
 	PERIPHS_IO_MUX_MTDO_U,
-	0                           /*< GPIO_16 should be configured other way, 
-	                             because it related to RTC module */
+	PAD_XPD_DCDC_CONF
 };
 
-// Absent RTC module definitions
+// RTC pin functions definitions
 #define FUNC_RTC_XPD_DCDC 0
 #define FUNC_RTC_GPIO0    1
 #define FUNC_EXT_WAKEUP   2
 #define FUNC_DEEPSLEEP    3
 #define FUNC_BT_XTAL_EN   4
 
-static const uint32_t func_list[PIN_QTY][5] =
+static const uint32_t func_list[pins][5] =
 {
-//  DO/DI         AF1,            AF2,            AF3,             AF4
-	{FUNC_GPIO0,  FUNC_SPICS2,    0,              0,               FUNC_CLK_OUT},
-	{FUNC_GPIO1,  FUNC_U0TXD,     FUNC_SPICS1,    0,               FUNC_CLK_RTC_BK},
-	{FUNC_GPIO2,  FUNC_I2SO_WS,   FUNC_U1TXD_BK,  0,               FUNC_U0TXD_BK},
-	{FUNC_GPIO3,  FUNC_U0RXD,     FUNC_I2SO_DATA, 0,               FUNC_CLK_XTAL_BK},
-	{FUNC_GPIO4,  FUNC_CLK_XTAL,  0,              0,               0},
-	{FUNC_GPIO5,  FUNC_CLK_RTC,   0,              0,               0},
-	{FUNC_GPIO6,  FUNC_SDCLK,     FUNC_SPICLK,    0,               UART1_CTS},
-	{FUNC_GPIO7,  FUNC_SDDATA0,   FUNC_SPIQ_MISO, 0,               FUNC_U1TXD},
-	{FUNC_GPIO8,  FUNC_SDDATA1,   FUNC_SPID_MOSI, 0,               FUNC_U1RXD},
-	{FUNC_GPIO9,  FUNC_SDDATA2,   FUNC_SPIHD,     0,               UFNC_HSPIHD},
-	{FUNC_GPIO10, FUNC_SDDATA3,   FUNC_SPIWP,     0,               FUNC_HSPIWP},
-	{FUNC_GPIO11, FUNC_SDCMD,     FUNC_SPICS0,    0,               UART1_RTS},
-	{FUNC_GPIO12, FUNC_MTDI,      FUNC_I2SI_DATA, FUNC_HSPIQ_MISO, FUNC_UART0_DTR},
-	{FUNC_GPIO13, FUNC_MTCK,      FUNC_I2SI_BCK,  FUNC_HSPID_MOSI, FUNC_UART0_CTS},
-	{FUNC_GPIO14, FUNC_MTMS,      FUNC_I2SI_WS,   FUNC_HSPI_CLK,   FUNC_UART0_DSR},
-	{FUNC_GPIO15, FUNC_MTDO,      FUNC_I2SO_BCK,  FUNC_HSPI_CS0,   FUNC_U0RTS},
-	{0,           0,              0,              0,               0}
+//  DO/DI            AF1,               AF2,             AF3,             AF4
+	{FUNC_GPIO0,     FUNC_SPICS2,       0,               0,               FUNC_CLK_OUT},
+	{FUNC_GPIO1,     FUNC_U0TXD,        FUNC_SPICS1,     0,               FUNC_CLK_RTC_BK},
+	{FUNC_GPIO2,     FUNC_I2SO_WS,      FUNC_U1TXD_BK,   0,               FUNC_U0TXD_BK},
+	{FUNC_GPIO3,     FUNC_U0RXD,        FUNC_I2SO_DATA,  0,               FUNC_CLK_XTAL_BK},
+	{FUNC_GPIO4,     FUNC_CLK_XTAL,     0,               0,               0},
+	{FUNC_GPIO5,     FUNC_CLK_RTC,      0,               0,               0},
+	{FUNC_GPIO6,     FUNC_SDCLK,        FUNC_SPICLK,     0,               UART1_CTS},
+	{FUNC_GPIO7,     FUNC_SDDATA0,      FUNC_SPIQ_MISO,  0,               FUNC_U1TXD},
+	{FUNC_GPIO8,     FUNC_SDDATA1,      FUNC_SPID_MOSI,  0,               FUNC_U1RXD},
+	{FUNC_GPIO9,     FUNC_SDDATA2,      FUNC_SPIHD,      0,               UFNC_HSPIHD},
+	{FUNC_GPIO10,    FUNC_SDDATA3,      FUNC_SPIWP,      0,               FUNC_HSPIWP},
+	{FUNC_GPIO11,    FUNC_SDCMD,        FUNC_SPICS0,     0,               U1RTS},
+	{FUNC_GPIO12,    FUNC_MTDI,         FUNC_I2SI_DATA,  FUNC_HSPIQ_MISO, FUNC_UART0_DTR},
+	{FUNC_GPIO13,    FUNC_MTCK,         FUNC_I2SI_BCK,   FUNC_HSPID_MOSI, FUNC_UART0_CTS},
+	{FUNC_GPIO14,    FUNC_MTMS,         FUNC_I2SI_WS,    FUNC_HSPI_CLK,   FUNC_UART0_DSR},
+	{FUNC_GPIO15,    FUNC_MTDO,         FUNC_I2SO_BCK,   FUNC_HSPI_CS0,   FUNC_U0RTS},
+	{FUNC_RTC_GPIO0, FUNC_RTC_XPD_DCDC, FUNC_EXT_WAKEUP, FUNC_DEEPSLEEP,  FUNC_BT_XTAL_EN}
 };
 
 gpio::gpio(uint8_t port, uint8_t pin, mode_t mode, bool state):
@@ -64,13 +67,8 @@ gpio::gpio(uint8_t port, uint8_t pin, mode_t mode, bool state):
 	_pin(pin),
 	_mode(mode)
 {
-	ASSERT(_port < PORT_QTY);
-	ASSERT(_pin < PIN_QTY);
-	ASSERT(_mode <= MODE_AF4);
-	// GPIO0-15 have only pulldown in input mode
-	ASSERT(_mode != MODE_DI || _pin > 15 || state);
-	// GPIO16 has only pullup in input mode
-	ASSERT(_mode != MODE_DI || _pin < 16 || !state);
+	ASSERT(_port < ports);
+	ASSERT(_pin < pins);
 	
 	gpio::mode(_mode, state);
 }
@@ -84,15 +82,19 @@ void gpio::set(bool state) const
 {
 	ASSERT(_mode == MODE_DO);
 	
-	if(_pin < 16)
+	if(_pin != rtc_pin)
 	{
-		GPIO_REG_WRITE((state ? GPIO_OUT_W1TS_ADDRESS : GPIO_OUT_W1TC_ADDRESS),
-			1 << _pin);
+		if(state)
+			GPIO.out_w1ts |= 1 << _pin;
+		else
+			GPIO.out_w1tc |= 1 << _pin;
 	}
 	else
 	{
-		uint32_t tmp_val = READ_PERI_REG(RTC_GPIO_OUT);
-		WRITE_PERI_REG(RTC_GPIO_OUT, (tmp_val & 0xfffffffe) | state);
+		if(state)
+			SET_PERI_REG_MASK(RTC_GPIO_OUT, 1);
+		else
+			CLEAR_PERI_REG_MASK(RTC_GPIO_OUT, 1);
 	}
 }
 
@@ -100,89 +102,78 @@ bool gpio::get() const
 {
 	ASSERT(_mode == MODE_DI && _mode == MODE_DO);
 	
-	if(_pin < 16)
-		return (bool)(GPIO_REG_READ(GPIO_IN_ADDRESS) & (1 << _pin));
+	if(_pin != rtc_pin)
+		return GPIO.in & (1 << _pin);
 	else
-		return (bool)(READ_PERI_REG(RTC_GPIO_IN_DATA) & 1);
+		return READ_PERI_REG(RTC_GPIO_IN_DATA) & 1;
 }
 
 void gpio::toggle() const
 {
 	ASSERT(_mode == MODE_DO);
 	
-	if(_pin < 16)
+	if(_pin != rtc_pin)
 	{
-		bool state = GPIO_REG_READ(GPIO_IN_ADDRESS) & (1 << _pin);
-		
-		GPIO_REG_WRITE((state ? GPIO_OUT_W1TC_ADDRESS : GPIO_OUT_W1TS_ADDRESS),
-			1 << _pin);
+		if(GPIO.in & (1 << _pin))
+			GPIO.out_w1tc |= 1 << _pin;
+		else
+			GPIO.out_w1ts |= 1 << _pin;
 	}
 	else
 	{
-		bool state = READ_PERI_REG(RTC_GPIO_IN_DATA) & 1;
-		
-		uint32_t tmp_val = READ_PERI_REG(RTC_GPIO_OUT);
-		WRITE_PERI_REG(RTC_GPIO_OUT, (tmp_val & 0xfffffffe) | ~state);
+		if(READ_PERI_REG(RTC_GPIO_IN_DATA) & 1)
+			CLEAR_PERI_REG_MASK(RTC_GPIO_OUT, 1);
+		else
+			SET_PERI_REG_MASK(RTC_GPIO_OUT, 1);
 	}
 }
 
 void gpio::mode(mode_t mode, bool state)
 {
 	ASSERT(_mode <= MODE_AF4);
-	// GPIO0-15 have only pulldown in input mode
-	ASSERT(_mode != MODE_DI || _pin > 15 || state);
-	// GPIO16 has only pullup in input mode
-	ASSERT(_mode != MODE_DI || _pin < 16 || !state);
+	if(_pin == rtc_pin)
+	{
+		ASSERT(_mode != MODE_DI || !state); // GPIO16 has only pulldown input mode
+		ASSERT(_mode != MODE_OD); // GPIO16 doesn't have open drain mode
+	}
 	
 	switch(_mode)
 	{
 		case MODE_DO:
-			if(_pin < 16)
+			if(_pin != rtc_pin)
 			{
-				// Set as GPIO
 				PIN_FUNC_SELECT(mux_list[_pin], func_list[_pin][0]);
-				
-				// Switch to output
-				GPIO_REG_WRITE(GPIO_ENABLE_W1TS_ADDRESS, 1 << _pin);
-				
-				// Setup default state
-				GPIO_REG_WRITE((state ? GPIO_OUT_W1TS_ADDRESS :
-					GPIO_OUT_W1TC_ADDRESS), 1 << _pin);
+				GPIO.enable_w1ts |= 1 << _pin;
+				GPIO.pin[_pin].driver = 0;
+				set(state);
 			}
 			else
 			{
 				// mux configuration for XPD_DCDC to output rtc_gpio0
-				uint32_t tmp_val = READ_PERI_REG(PAD_XPD_DCDC_CONF);
-				WRITE_PERI_REG(PAD_XPD_DCDC_CONF, (tmp_val & 0xffffffbc) | 0x1);
+				uint32_t tmp_val = READ_PERI_REG(mux_list[_pin]) & 0xffffffbc;
+				WRITE_PERI_REG(mux_list[_pin], tmp_val | 1);
 				
-				// mux configuration for out enable
-				tmp_val = READ_PERI_REG(RTC_GPIO_CONF);
-				WRITE_PERI_REG(RTC_GPIO_CONF, tmp_val & 0xfffffffe);
-				
-				// Out enable
-				tmp_val = READ_PERI_REG(RTC_GPIO_ENABLE);
-				WRITE_PERI_REG(RTC_GPIO_ENABLE, (tmp_val & 0xfffffffe) | 0x1);
-				
-				// Setup default state
-				tmp_val = READ_PERI_REG(RTC_GPIO_OUT);
-				WRITE_PERI_REG(RTC_GPIO_OUT, (tmp_val & 0xfffffffe) | state);
+				CLEAR_PERI_REG_MASK(RTC_GPIO_CONF, 1);
+				SET_PERI_REG_MASK(RTC_GPIO_ENABLE, 1);
+				set(state);
 			}
 			break;
 		
 		case MODE_OD:
-			// TODO
+			PIN_FUNC_SELECT(mux_list[_pin], func_list[_pin][0]);
+			GPIO.enable_w1ts |= 1 << _pin;
+			GPIO.pin[_pin].driver = 1;
+			set(state);
 			break;
 		
 		case MODE_DI:
-			if(_pin < 16)
+			if(_pin != rtc_pin)
 			{
-				// Set as GPIO
 				PIN_FUNC_SELECT(mux_list[_pin], func_list[_pin][0]);
+				GPIO.enable_w1tc |= 1 << _pin;
+				GPIO.pin[_pin].driver = 0;
 				
-				// Switch to input
-				GPIO_REG_WRITE(GPIO_ENABLE_W1TC_ADDRESS, 1 << _pin);
-				
-				// Setup pullup or pulldown
+				// GPIO 0-15 has pullup/floating input modes only
 				if(state)
 					PIN_PULLUP_EN(mux_list[_pin]);
 				else
@@ -191,16 +182,16 @@ void gpio::mode(mode_t mode, bool state)
 			else
 			{
 				// mux configuration for XPD_DCDC and rtc_gpio0 connection
-				uint32_t tmp_val = READ_PERI_REG(PAD_XPD_DCDC_CONF);
-				WRITE_PERI_REG(PAD_XPD_DCDC_CONF, (tmp_val & 0xffffffbc) | 0x1);
+				uint32_t tmp_val = READ_PERI_REG(mux_list[_pin]) & 0xffffffbc;
+				WRITE_PERI_REG(mux_list[_pin], tmp_val | 1);
 				
-				//mux configuration for out enable
-				tmp_val = READ_PERI_REG(RTC_GPIO_CONF);
-				WRITE_PERI_REG(RTC_GPIO_CONF, tmp_val & 0xfffffffe);
+				CLEAR_PERI_REG_MASK(RTC_GPIO_CONF, 1);
+				CLEAR_PERI_REG_MASK(RTC_GPIO_ENABLE, 1);
 				
-				// Out disable
-				tmp_val = READ_PERI_REG(RTC_GPIO_ENABLE);
-				WRITE_PERI_REG(RTC_GPIO_ENABLE, tmp_val & 0xfffffffe);
+				// GPIO 16 has pulldown/floating input modes only
+				gpio_pin_reg_t pin_reg = {.val = READ_PERI_REG(mux_list[_pin])};
+				pin_reg.rtc_pin.pulldown = !state;
+				WRITE_PERI_REG(mux_list[_pin], pin_reg.val);
 			}
 			break;
 		
@@ -208,10 +199,10 @@ void gpio::mode(mode_t mode, bool state)
 		case MODE_AF2:
 		case MODE_AF3:
 		case MODE_AF4:
-			if(_pin < 16)
+			if(_pin != rtc_pin)
 			{
-				// Set as AFx
-				PIN_FUNC_SELECT(mux_list[_pin], func_list[_pin][_mode - 1]);
+				// Normalize _mode (- MODE_DI) to access proper row from func_list
+				PIN_FUNC_SELECT(mux_list[_pin], func_list[_pin][_mode - MODE_DI]);
 			}
 			else
 			{
--- src/hal/ESP8266/gpio/gpio.hpp
+++ src/hal/ESP8266/gpio/gpio.hpp
@@ -2,34 +2,31 @@
 
 #include <stdint.h>
 
-/* List of ESP8266 pin functions
-| GPIO | Default  |   Func1   |   Func2    |   Func3    |    Func4    |
-| ---: | -------- | --------- | ---------- | ---------- | ----------- |
-|    0 | GPIO0    | SPICS2    | -          | -          | CLK_OUT     |
-|    1 | U0TXD    | SPICS1    | -          | GPIO1      | CLK_RTC_BK  |
-|    2 | GPIO2    | I2SO_WS   | U1TXD_BK   | -          | U0TXD_BK    |
-|    3 | U0RXD    | I2SO_DATA | -          | GPIO3      | CLK_XTAL_BK |
-|    4 | GPIO4    | CLK_XTAL  | -          | -          | -           |
-|    5 | GPIO5    | CLK_RTC   | -          | -          | -           |
-|   *6 | SDCLK    | SPICLK    | -          | GPIO6      | U1CTS       |
-|   *7 | SDDATA0  | SPIQ_MISO | -          | GPIO7      | U1TXD       |
-|   *8 | SDDATA1  | SPID_MOSI | -          | GPIO8      | U1RXD       |
-|   *9 | SDDATA2  | SPIHD     | -          | GPIO9      | HSPIHD      |
-|  *10 | SDDATA3  | SPIWP     | -          | GPIO10     | HSPIWP      |
-|  *11 | SDCMD    | SPICS0    | -          | GPIO11     | U1RTS       |
-|   12 | MTDI     | I2SI_DATA | HSPIQ_MISO | GPIO12     | UART0_DTR   |
-|   13 | MTCK     | I2SI_BCK  | HSPID_MOSI | GPIO13     | UART0_CTS   |
-|   14 | MTMS     | I2SI_WS   | HSPI_CLK   | GPIO14     | UART0_DSR   |
-|   15 | MTDO     | I2SO_BCK  | HSPI_CS0   | GPIO15     | U0RTS       |
-|   16 | XPD_DCDC | RTC_GPIO0 | EXT_WAKEUP | DEEPSLEEP  | BT_XTAL_EN  |
-| ---- | -------- | --------- | ---------- | ---------- | ----------- |
+/* List of ESP8266 pin functions according to this hal:
+| DO,OD,DI |    AF1   |    AF2     |    AF3     |    AF4      |
+| -------- | -------- | ---------- | ---------- | ----------- |
+| GPIO0    | SPICS2   | -          | -          | CLK_OUT     |
+| GPIO1    | U0TXD    | SPICS1     | -          | CLK_RTC_BK  |
+| GPIO2    | I2SO_WS  | U1TXD_BK   | -          | U0TXD_BK    |
+| GPIO3    | U0RXD    | I2SO_DATA  | -          | CLK_XTAL_BK |
+| GPIO4    | CLK_XTAL | -          | -          | -           |
+| GPIO5    | CLK_RTC  | -          | -          | -           |
+| GPIO6*   | SDCLK    | SPICLK     | -          | U1CTS       |
+| GPIO7*   | SDDATA0  | SPIQ_MISO  | -          | U1TXD       |
+| GPIO8*   | SDDATA1  | SPID_MOSI  | -          | U1RXD       |
+| GPIO9*   | SDDATA2  | SPIHD      | -          | HSPIHD      |
+| GPIO10*  | SDDATA3  | SPIWP      | -          | HSPIWP      |
+| GPIO11*  | SDCMD    | SPICS0     | -          | U1RTS       |
+| GPIO12   | MTDI     | I2SI_DATA  | HSPIQ_MISO | U0DTR       |
+| GPIO13   | MTCK     | I2SI_BCK   | HSPID_MOSI | U0CTS       |
+| GPIO14   | MTMS     | I2SI_WS    | HSPI_CLK   | U0DSR       |
+| GPIO15   | MTDO     | I2SO_BCK   | HSPI_CS    | U0RTS       |
+| GPIO16   | XPD_DCDC | EXT_WAKEUP | DEEPSLEEP  | BT_XTAL_EN  |
+| -------- | -------- | ---------- | ---------- | ----------- |
 * - pins used for accessing external flash memory */
 
 namespace hal
 {
-#define PORT_QTY 1
-#define PIN_QTY  17
-
 class gpio
 {
 	public:
