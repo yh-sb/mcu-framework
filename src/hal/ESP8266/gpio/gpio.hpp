97cb1d441423eaeac5c4d4094369f3f915b4151e
r44083 <r44083.r@gmail.com>
2019-09-21 17:07:07 +0300

    Update gpio list doc for ESP8266 hal

--- src/hal/ESP8266/gpio/gpio.hpp
+++ src/hal/ESP8266/gpio/gpio.hpp
@@ -2,51 +2,28 @@
 
 #include <stdint.h>
 
-/*
-List of ESP8266 pin functions from Espressif documentation:
-| GPIO |    Name    |  Func1   |   Func2   |   Func3    |   Func4   |   Func5    |
-| ---: | ---------- | -------- | --------- | ---------- | --------- | ---------- |
-|    0 | GPIO0_U    | GPIO0    | SPICS2    | -          | -         | CLK_OUT    |
-|    1 | U0TXD_U    | U0TXD    | SPICS1    | -          | GPIO1     | CLK_RTC    |
-|    2 | GPIO2_U    | GPIO2    | I2SO_WS   | U1TXD      | -         | U0TXD      |
-|    3 | U0RXD_U    | U0RXD    | I2SO_DATA | -          | GPIO3     | CLK_XTAL   |
-|    4 | GPIO4_U    | GPIO4    | CLK_XTAL  | -          | -         | -          |
-|    5 | GPIO5_U    | GPIO5    | CLK_RTC   | -          | -         | -          |
-|   *6 | SD_CLK_U   | SD_CLK   | SPICLK    | -          | GPIO6     | U1CTS      |
-|   *7 | SD_DATA0_U | SD_DATA0 | SPIQ      | -          | GPIO7     | U1TXD      |
-|   *8 | SD_DATA1_U | SD_DATA1 | SPID      | -          | GPIO8     | U1RXD      |
-|   *9 | SD_DATA2_U | SD_DATA2 | SPIHD     | -          | GPIO9     | HSPIHD     |
-|  *10 | SD_DATA3_U | SD_DATA3 | SPIWP     | -          | GPIO10    | HSPIWP     |
-|  *11 | SD_CMD_U   | SD_CMD   | SPICS0    | -          | GPIO11    | U1RTS      |
-|   12 | MTDI_U     | MTDI     | I2SI_DATA | HSPIQ MISO | GPIO12    | U0DTR      |
-|   13 | MTCK_U     | MTCK     | I2SI_BCK  | HSPID MOSI | GPIO13    | U0CTS      |
-|   14 | MTMS_U     | MTMS     | I2SI_WS   | HSPICLK    | GPIO14    | U0DSR      |
-|   15 | MTDO_U     | MTDO     | I2SO_BCK  | HSPICS     | GPIO15    | U0RTS      |
-|   16 | XPD_DCDC   | XPD_DCDC | RTC_GPIO0 | EXT_WAKEUP | DEEPSLEEP | BT_XTAL_EN |
-| ---- | ---------- | -------- | --------- | ---------- | --------- | ---------- |
-
-List of ESP8266 pin functions according to this hal_gpio:
-| GPIO |    AF1    |    AF2     |    AF3     |    AF4     |
-| ---: | --------- | ---------- | ---------- | ---------- |
-|    0 | SPICS2    | -          | -          | CLK_OUT    |
-|    1 | U0TXD     | SPICS1     | -          | CLK_RTC1   |
-|    2 | I2SO_WS   | U1TXD      | -          | U0TXD      |
-|    3 | U0RXD     | I2SO_DATA  | -          | CLK_XTAL1  |
-|    4 | CLK_XTAL2 | -          | -          | -          |
-|    5 | CLK_RTC2  | -          | -          | -          |
-|   *6 | SD_CLK    | SPICLK     | -          | U1CTS      |
-|   *7 | SD_DATA0  | SPIQ       | -          | U1TXD      |
-|   *8 | SD_DATA1  | SPID       | -          | U1RXD      |
-|   *9 | SD_DATA2  | SPIHD      | -          | HSPIHD     |
-|  *10 | SD_DATA3  | SPIWP      | -          | HSPIWP     |
-|  *11 | SD_CMD    | SPICS0     | -          | U1RTS      |
-|   12 | MTDI      | I2SI_DATA  | HSPIQ MISO | U0DTR      |
-|   13 | MTCK      | I2SI_BCK   | HSPID MOSI | U0CTS      |
-|   14 | MTMS      | I2SI_WS    | HSPICLK    | U0DSR      |
-|   15 | MTDO      | I2SO_BCK   | HSPICS     | U0RTS      |
-|   16 | XPD_DCDC  | EXT_WAKEUP | DEEPSLEEP  | BT_XTAL_EN |
-| ---- | --------- | ---------- | ---------- | ---------- |
-*/
+/* List of ESP8266 pin functions
+| GPIO | Default  |   Func1   |   Func2    |   Func3    |    Func4    |
+| ---: | -------- | --------- | ---------- | ---------- | ----------- |
+|    0 | GPIO0    | SPICS2    | -          | -          | CLK_OUT     |
+|    1 | U0TXD    | SPICS1    | -          | GPIO1      | CLK_RTC_BK  |
+|    2 | GPIO2    | I2SO_WS   | U1TXD_BK   | -          | U0TXD_BK    |
+|    3 | U0RXD    | I2SO_DATA | -          | GPIO3      | CLK_XTAL_BK |
+|    4 | GPIO4    | CLK_XTAL  | -          | -          | -           |
+|    5 | GPIO5    | CLK_RTC   | -          | -          | -           |
+|   *6 | SDCLK    | SPICLK    | -          | GPIO6      | U1CTS       |
+|   *7 | SDDATA0  | SPIQ_MISO | -          | GPIO7      | U1TXD       |
+|   *8 | SDDATA1  | SPID_MOSI | -          | GPIO8      | U1RXD       |
+|   *9 | SDDATA2  | SPIHD     | -          | GPIO9      | HSPIHD      |
+|  *10 | SDDATA3  | SPIWP     | -          | GPIO10     | HSPIWP      |
+|  *11 | SDCMD    | SPICS0    | -          | GPIO11     | U1RTS       |
+|   12 | MTDI     | I2SI_DATA | HSPIQ_MISO | GPIO12     | UART0_DTR   |
+|   13 | MTCK     | I2SI_BCK  | HSPID_MOSI | GPIO13     | UART0_CTS   |
+|   14 | MTMS     | I2SI_WS   | HSPI_CLK   | GPIO14     | UART0_DSR   |
+|   15 | MTDO     | I2SO_BCK  | HSPI_CS0   | GPIO15     | U0RTS       |
+|   16 | XPD_DCDC | RTC_GPIO0 | EXT_WAKEUP | DEEPSLEEP  | BT_XTAL_EN  |
+| ---- | -------- | --------- | ---------- | ---------- | ----------- |
+* - pins used for accessing external flash memory */
 
 namespace hal
 {
