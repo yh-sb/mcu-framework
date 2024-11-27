dbbda67f0aa28fb13e7ff61ff77467ee33075b6f
yh-sb <yh.sb@outlook.com>
2024-11-27 11:27:09 +0200

    Check for FreeRTOS runtime stack overflow only in Debug mode

--- periph/stm32f0/include/FreeRTOSConfig.h
+++ periph/stm32f0/include/FreeRTOSConfig.h
@@ -61,7 +61,7 @@ extern uint32_t SystemCoreClock;
 #define configIDLE_SHOULD_YIELD         1
 #define configUSE_MUTEXES               1
 #define configQUEUE_REGISTRY_SIZE       8
-#define configCHECK_FOR_STACK_OVERFLOW  2
+#define configCHECK_FOR_STACK_OVERFLOW  (defined(NDEBUG) ? 0 : 2)
 #define configUSE_RECURSIVE_MUTEXES     1
 #define configUSE_MALLOC_FAILED_HOOK    1
 #define configUSE_APPLICATION_TASK_TAG  0
